0163d79f76946a0d0cf9d793e7cd60aff8e04a3f
Clemens Fruhwirth <clemens@endorphin.org>
2019-01-08 20:13:10 +0100

    Clean-up and more comments.

--- aes.py
+++ aes.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # Copyright 2004, 2019, Clemens Fruhwirth <clemens@endorphin.org>
 
-# My pedagocial Rijndael implementation in Python.
+# My pedagogicial Rijndael implementation in Python.
+# It uses a minimal amount of magic values (no predefined lookup
+# tables) and if so, they are marked with MAGIC
 
-# DO NOT USE IN PRODUCTION OR WITH ANY PRODUCTION SECRETS THIS IS MY
+# DO NOT USE IN PRODUCTION OR WITH ANY PRODUCTION SECRETS. THIS IS MY
 # TOY IMPLEMENTATION OF AES, WHICH I WROTE IN 2004 TO LEARN PYTHON AND
 # AES.
 
@@ -18,10 +20,11 @@ logger = logging.getLogger("aes.py")
 
 # Residue class 2 field
 Z2 = Z(2)
+
 # Polynomial over field Z2
 POFZ2 = POF(Z2)
 
-# Reduction polynomial in POFZ2 as defined by Page 36 of the Rijndael book.
+# Reduction polynomial in POFZ2 as defined by Page 36 of the Rijndael book. MAGIC
 rp = L2POL([1, 1, 0, 1, 1, 0, 0, 0, 1], Z2)
 
 # Galois field over Z2 with reduction polynomial
@@ -56,11 +59,12 @@ def fGen(a, mask):
 
 def f(a):
   """f, as defined by the Rijndael book Page 36."""
-  # As defined by page 36. in the Rijndael book.
+  # MAGIC
   return fGen(a, 0xF1) ^ 0x63
 
 def fInv(a):
   """f^-1, as defined by the Rijndael book Page 37."""
+  # MAGIC
   return fGen(a, 0xA4) ^ 0x05
 
 def g(a):
@@ -122,6 +126,7 @@ def SubBytes(state, function):
     r.append(map(function, i))
   return r
 
+# MAGIC
 ShiftRowsOffsets = [
   [0, 1, 2, 3],
   [0, 1, 2, 3],
@@ -264,20 +269,26 @@ def dumpStateHex(state):
   s += "]"
   return s
 
-# D.2 Rijndael test vectors
-key = [
-  0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
-  0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
-msg = [
-  0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d,
-  0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34]
 
 if __name__ == '__main__':
+  # D.2 Rijndael test vectors
+  # This enc test vector comes from Page 216 in the Rijndael
+  key = [
+    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+    0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
+
+  plaintext = [
+    0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d,
+    0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34]
+
+  # Labelled R[10].output in output
+  ciphertext = [ 0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb,
+                 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0x0b, 0x32]
   logger.setLevel(logging.DEBUG)
-  enc = rijndael(arrayToState(msg), arrayToState(key))
-  # This enc test vector comes from Page 216 in the Rijndael book labelled R[10].output
-  print "Test vector check:  ", enc == [0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0x0b, 0x32]
+
+  enc = rijndael(arrayToState(plaintext), arrayToState(key))
+  print "Test vector check: ", enc == ciphertext
 
   dec = invRijndael(arrayToState(enc), arrayToState(key))
-  print "Encryption symmetry:", msg == dec
+  print "Encryption symmetry:", plaintext == dec
 
