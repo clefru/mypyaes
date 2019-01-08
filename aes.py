3f646035faf436c5f1bfb78dcd4c95d38ad4ed02
Clemens Fruhwirth <clemens@endorphin.org>
2019-01-08 15:59:58 +0100

    Add key expansion for keysize >= 224bit
    And refactor function.

--- aes.py
+++ aes.py
@@ -97,18 +97,19 @@ def keyExpansion(cipherKey, nr, nk, nb):
   expandedKey = []
   for j in range(0, nk):
     expandedKey.append(cipherKey[j])
-  for j in range(nk,  nb*(nr+1)):
+  for j in range(nk, nb*(nr+1)):
+    sub = []
     if j%nk == 0:
-      sub = []
       sub.append(expandedKey[j-nk][0] ^ SR(expandedKey[j-1][1]) ^ RC(j/nk))
       for i in range(1, 4):
         sub.append(expandedKey[j-nk][i] ^ SR(expandedKey[j-1][(i+1)%4]))
-      expandedKey.append(sub)
+    elif j%nk == 4 and nk > 6:
+      for i in range(0, 4):
+        sub.append(expandedKey[j-nk][i] ^ SR(expandedKey[j-1][i]))
     else:
-      sub = []
       for i in range(0, 4):
         sub.append(expandedKey[j-nk][i] ^ expandedKey[j-1][i])
-      expandedKey.append(sub)
+    expandedKey.append(sub)
   return expandedKey
 
 def SubBytes(state, function):
